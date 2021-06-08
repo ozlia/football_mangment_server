cfa6eb4201cfe1d34e5e2afc2b3c4a04d004e456
avichai edri <avichaie@post.bgu.ac.il>
2021-06-08 18:27:06 +0300

    add test

--- match.test.js
+++ match.test.js
@@ -81,3 +81,90 @@ test('add match invalid league test', async () => {
   }
 
 });
+
+test('extractRelevantData empty match_list test', async () => {
+    expect(async () => {
+      await extractRelevantData([]).toBe([]);
+    });
+});
+
+test('prePostMatches test', async () => {
+  try{
+  await addMatch({
+    home_team: 'test home team',
+    away_team: 'test away team',
+    league_id: 271,
+    season: '20-21',
+    stage: 'round 1',
+    court: 1,
+    referee_name: 'test ref',
+    date: "2021-06-08T16:00:00.000Z",
+    score: NaN});
+    await addMatch({
+      home_team: 'test home team',
+      away_team: 'test away team',
+      league_id: 271,
+      season: '20-21',
+      stage: 'round 1',
+      court: 1,
+      referee_name: 'test ref',
+      date: "2021-06-08T16:00:00.000Z",
+      score: '1:0'});
+    const pre_match = await DButils.execQuery(
+      `select * from match where
+      home_team = 'test home team' AND 
+      away_team = 'test away team' AND 
+      league = 271 AND 
+      season = '20-21' AND 
+      stage = 'round 1' AND 
+      court = 1 AND 
+      referee_name = 'test ref' AND 
+      date = '2021-06-08T16:00:00.000Z'`
+    );
+    const post_match = await DButils.execQuery(
+      `select * from match where
+      home_team = 'test home team' AND 
+      away_team = 'test away team' AND 
+      league = 271 AND 
+      season = '20-21' AND 
+      stage = 'round 1' AND 
+      court = 1 AND 
+      referee_name = 'test ref' AND 
+      date = '2020-06-08T16:00:00.000Z'`
+    );
+    expect(async () => {
+      await prePostMatches([pre_match,post_match]).toBe(
+        {pre_played_matches: [pre_match],
+         post_played_match: [post_match]});
+        });
+
+  }
+  catch{
+    throw("prePostMatches test failed")
+  }
+    finally{
+      await DButils.execQuery(
+          `DELETE FROM match WHERE 
+          home_team = 'test home team' AND 
+          away_team = 'test away team' AND 
+          league = 271 AND 
+          season = '20-21' AND 
+          stage = 'round 1' AND 
+          court = 1 AND 
+          referee_name = 'test ref' AND 
+          date = '2021-06-08T16:00:00.000Z'`
+        );
+        await DButils.execQuery(
+          `DELETE FROM match WHERE 
+          home_team = 'test home team' AND 
+          away_team = 'test away team' AND 
+          league = 271 AND 
+          season = '20-21' AND 
+          stage = 'round 1' AND 
+          court = 1 AND 
+          referee_name = 'test ref' AND 
+          date = '2020-06-08T16:00:00.000Z'`
+        );
+  }
+
+});
