f5b69204035ba1d1244c331a1ec759fcff1a9229
tomRob123 <59135978+tomRob123@users.noreply.github.com>
2021-06-09 15:17:37 +0300

    match constrains split to functions

--- project/routes/utils/union_rep_utils.js
+++ project/routes/utils/union_rep_utils.js
@@ -3,29 +3,38 @@ const match_utils = require("./match_utils");
 const team_utils = require("./team_utils");
 const referee_utils = require("./referee_utils");
 
+// make sure no games are set in this day
+async function verifyTeamsFreeOnMatchDate(home_team_name,away_team_name,date){
+    let number_of_home_team_matches = await team_utils.teamMatchesOnDay(home_team_name, date);
+    let number_of_away_team_matches = await team_utils.teamMatchesOnDay(away_team_name, date);
+    if (number_of_home_team_matches.length > 0 || number_of_away_team_matches.length > 0){
+        throw({status:400, message: "invalid match day"});
+    }
+
+}
+
+// make sure ref is in league //todo maybe should ask league for all his refree or get ref by his name
+async function verifyRefInLeague(ref_name){
+    let ref_league = (await referee_utils.getRefLeague(ref_name));
+    if (ref_league == 0 || ref_league[0].league_id != league_id){
+        throw({status: 404, message: "referee is not in this league"});
+    }
+}
+
 
 async function checkMatchCreationConstraints(home_team_name, away_team_name, ref_name, date) {
     
-        // make sure both teams are in said league
+        
         try{
             let league_id = await league_utils.getLeagueId();
+            
+            // make sure both teams are in said league
             let ht = await league_utils.getTeamByLeague(home_team_name, league_id);
             let at = await league_utils.getTeamByLeague(away_team_name, league_id);
 
-            // make sure no games are set in this day
-            
-            let number_of_home_team_matches = await team_utils.teamMatchesOnDay(home_team_name, date);
-            let number_of_away_team_matches = await team_utils.teamMatchesOnDay(away_team_name, date);
-            if (number_of_home_team_matches.length > 0 || number_of_away_team_matches.length > 0){
-                throw({status:400, message: "invalid match day"});
-            }
-
-            // make sure ref is in league //todo maybe should ask league for all his refree or get ref by his name
-            
-            let ref_league = (await referee_utils.getRefLeague(ref_name));
-            if (ref_league == 0 || ref_league[0].league_id != league_id){
-                throw({status: 404, message: "referee is not in this league"});
-            }
+            await verifyTeamsFreeOnMatchDate(home_team_name,away_team_name,date);
+            await verifyRefInLeague(ref_name);
+    
             
             // check court of home team
             let match_court = ht.venue_id
