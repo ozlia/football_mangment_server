f4f9540173f9da71927b0dcb17dae77b0b8f3d2e
ozlia <63154309+ozlia@users.noreply.github.com>
2021-06-10 00:38:22 +0300

    authCobtroller sys test

--- project/__tests__/system/authController.test.js
+++ project/__tests__/system/authController.test.js
@@ -10,12 +10,19 @@ const reg_user = {
     "email": "Suban@gmail.com",
     "profile picture": "https://res.cloudinary.com/dswkzsdoq/image/upload/v1620738869/20171122_132608_zvrw97.jpg"
   };
-describe("GET /alive", ()=> {
 
-    describe("checking server is alive", () => {
-        test("alve checker", async()=> {
-        const res = await request(app.app).get("/alive").send();
-        expect(res.text).toBe("I'm alive");
+describe("/auth", ()=> {
+
+    describe("/regiser", () => {
+        test("basic register", async()=> {
+        const res = await request(app.app).post("/auth/register").send(reg_user);
+        expect(res.statusCode).toBe(201);
         })
     })
 })
+
+afterAll(async () => {
+  await DButils.execQuery(`DELETE FROM users WHERE username = 'KingMessi'`);
+  await DButils.pool.close();
+});
+
